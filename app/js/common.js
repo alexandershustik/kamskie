a73ef83a569baf868cb03804ea07b2b7bfc50211
Alexander Shustik <alexandershustik@gmail.com>
2017-02-15 12:37:41 +0200

    Main atwork

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,18 @@
 $( document ).ready(function() {
 	$( '.menu-btn' ).click(function(){
-		$( '.menu-block' ).toggleClass('open');
+		$( '.menu-block' ).toggleClass( 'open' );
 	});
 	$( '.menu-close-btn' ).click(function(){
-		$( '.menu-block' ).removeClass('open');
+		$( '.menu-block' ).removeClass( 'open' );
+	});
+	$( '.icons-wrap span' ).click(function(){
+		$( '.icons-wrap span' ).removeClass( 'active' );
+		$( this ).addClass( 'active' );
+
+		$( '.wrapper' ).removeClass( 'day night' )
+
+		$( this ).hasClass( 'icon-sun' ) ?
+			$( '.wrapper' ).addClass( 'day' ) :
+			$( '.wrapper' ).addClass( 'night' );
 	});
 });
