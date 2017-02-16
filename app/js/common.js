04005aaecf0add3bccc9fdc5d852d6ccf4184f74
Alexander Shustik <alexandershustik@gmail.com>
2017-02-16 08:34:28 +0200

    верстка

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,12 @@
 $( document ).ready(function() {
+	//menu
 	$( '.menu-btn' ).click(function(){
 		$( '.menu-block' ).toggleClass( 'open' );
 	});
 	$( '.menu-close-btn' ).click(function(){
 		$( '.menu-block' ).removeClass( 'open' );
 	});
+	//day-night
 	$( '.icons-wrap span' ).click(function(){
 		$( '.icons-wrap span' ).removeClass( 'active' );
 		$( this ).addClass( 'active' );
